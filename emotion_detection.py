8e6035e578589e405437943c106dcdb63bbb94d4
AmerFarhat <amerfarhat2014@gmail.com>
2025-01-19 15:07:49 -0500

    2nd Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -7,4 +8,19 @@ headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_sto
 def emotion_detector(text_to_analyze):
     input_json ={ "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers=headers, json = input_json)
-    return response.json()
+    response_data = response.json()
+
+    # emotion scores dictionary
+    predictions = response_data.get("emotionPredictions",[])
+    emotions = predictions[0].get("emotion", {})
+
+    # Extract the required emotions from the API response
+    relevant_emotions = {emotion: emotions.get(emotion) for emotion in["anger", "disgust", "fear", "joy", "sadness"]}
+    
+    # Find the dominant emotion
+    dominant_emotion = max(relevant_emotions, key=relevant_emotions.get)
+
+    # Add the dominant emotion to the result
+    relevant_emotions["dominant_emotion"] = dominant_emotion
+
+    return relevant_emotions
